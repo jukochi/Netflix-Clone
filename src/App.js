02281d15f631aa8b4edbe68cb5507382470ddee7
Justin Kobe Chico <chicokobe@gmail.com>
2021-01-11 22:34:26 +0800

    final build netflix-clone

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Nav from "./Nav";
+import requests from "./requests";
+import Row from "./Row";
+import Banner from "./Banner";
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
